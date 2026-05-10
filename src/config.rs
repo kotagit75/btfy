c7bfaf3ab09785424e2a1e91b29a08a6e617cdf8
kotagit75 <unity75lets5@gmail.com>
2026-05-10 10:51:23 +0900

    added a feature to load the config from environment variables

--- src/config.rs
+++ src/config.rs
@@ -1,11 +1,21 @@
-use std::sync::LazyLock;
+use std::{env, sync::LazyLock};
 
 pub struct Config {
     pub api_port: u16,
     pub cors_allow_port: u16,
 }
 
-pub static CONFIG: LazyLock<Config> = LazyLock::new(|| Config {
-    api_port: 8080,
-    cors_allow_port: 3000,
+pub static CONFIG: LazyLock<Config> = LazyLock::new(|| {
+    let api_port = match env::var("API_PORT") {
+        Ok(val) => val.parse().unwrap(),
+        Err(_) => 8080,
+    };
+    let cors_allow_port = match env::var("CORS_ALLOW_PORT") {
+        Ok(val) => val.parse().unwrap(),
+        Err(_) => 3000,
+    };
+    Config {
+        api_port,
+        cors_allow_port,
+    }
 });
