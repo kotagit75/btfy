9dc31dbaadad952ea2d46c33faa2301492adcf5b
kotagit75 <unity75lets5@gmail.com>
2026-07-11 17:33:49 +0900

    feat: updated dummy.py

--- example/dummy.py
+++ example/dummy.py
@@ -1,30 +1,10 @@
 #!/usr/bin/env python3
-from http.server import BaseHTTPRequestHandler, ThreadingHTTPServer
-from urllib.parse import urlparse
-
-
-class Handler(BaseHTTPRequestHandler):
-    def do_GET(self):
-        u = urlparse(self.path)
-        if u.path != "/":
-            self.send_response(404)
-            self.end_headers()
-            return
-
-        body = "1".encode("utf-8")
-        self.send_response(200)
-        self.send_header("Content-Type", "text/plain; charset=utf-8")
-        self.send_header("Content-Length", str(len(body)))
-        self.end_headers()
-        self.wfile.write(body)
-
+import json
+import sys
 
 def main():
-    host = "127.0.0.1"
-    port = 8000
-    httpd = ThreadingHTTPServer((host, port), Handler)
-    print(f"temperature server: http://{host}:{port}/")
-    httpd.serve_forever()
+    for _ in sys.stdin:
+        print(json.dumps({"temperature": int(round(10))}), flush=True)
 
 
 if __name__ == "__main__":
