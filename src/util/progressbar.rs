0ee422679a864ca647cfee94757b3b0583d110e9
kotagit75 <unity75lets5@gmail.com>
2026-07-18 10:03:09 +0900

    feat: updated ProgressStyle

--- src/util/progressbar.rs
+++ src/util/progressbar.rs
@@ -4,7 +4,7 @@ pub fn get_progress_bar(len: u64) -> ProgressBar {
     let pb = ProgressBar::new(len);
     pb.set_style(
         ProgressStyle::with_template(
-            "[{elapsed_precise}] {bar:40.cyan/blue} {pos:>7}/{len:7} {msg}",
+            "[{elapsed_precise}] [{bar:40.cyan/blue}] {pos:>7}/{len:7} {msg}",
         )
         .unwrap()
         .progress_chars("##-"),
